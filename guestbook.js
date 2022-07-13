e68ff6d6feba0bdaae629e17e44b4685195ca505
Andrew93 <andrewnmuser@yahoo.com>
2022-07-13 00:18:55 +0000

    Added the apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://cc3fccd2.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
